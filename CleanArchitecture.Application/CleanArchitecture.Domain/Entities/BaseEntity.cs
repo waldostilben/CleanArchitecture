7c8edd0f8f1f3799a50dc30f96c52b7af0b017f9
Waldo Stilben <waldostilben@gmail.com>
2023-10-02 23:57:15 -0300

    add entities and interfaces classes in CleanArchitecture.Domain

---
+++ CleanArchitecture.Application/CleanArchitecture.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿
+namespace CleanArchitecture.Domain.Entities
+{
+    public abstract class BaseEntity //classe destinada a ser herdada por outras classes
+    {
+        public Guid Id { get; set; } //estrutura que representa um identificador de 128 bits
+        public DateTimeOffset DateCreated { get; set; } //DateTimeOffset é um DateTime universal UTC
+        public DateTimeOffset? DateUpdated { get; set; }
+        public DateTimeOffset? DateDeleted { get; set; }
+    }
+}
---
+++ CleanArchitecture.Application/CleanArchitecture.Domain/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿
+namespace CleanArchitecture.Domain.Entities
+{
+    public sealed class User : BaseEntity //sealed não permite que a classe seja herdada
+    {
+        public string? Email { get; set; }
+        public string? Name { get; set; }
+    }
+}
